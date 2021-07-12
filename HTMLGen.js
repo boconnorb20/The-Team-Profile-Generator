b4cc747b2263f91e08732ec2ed2c000b2d3d8254
Brian OConnor <boconnor20@gmail.com>
2021-07-12 17:04:30 -0500

    working progress on Employee.js,Engineer.js,index.js,Intern.js, installed dependancies, getting HTMLGen going

---
+++ HTMLGen.js
@@ -0,0 +1,68 @@
+const path = require("path");
+const fs = require("fs");
+
+const templatesDir = path.resolve(__dirname, "../templates");
+
+const display = (employees) => {
+  const html = [];
+
+  html.push(
+    employees
+      .filter((employee) => employee.getRole() === "Manager")
+      .map((manager) => displayManager(manager))
+  );
+
+  html.push(
+    employees
+      .filter((employee) => employee.getRole() === "Engineer")
+      .map((engineer) => displayEngineer(engineer))
+  );
+  html.push(
+    employees
+      .filter((employee) => employee.getRole() === "Intern")
+      .map((intern) => displayIntern(intern))
+  );
+  return displayMain(html.join(""));
+};
+
+const displayManager = (manager) => {
+  let template = fs.readFileSync(path.resolve(templatesDir, "manager.html"), "utf8");
+  template = replace(template, "name", manager.getName());
+  template = replace(template, "role", manager.getRole());
+  template = replace(template, "email", manager.getEmail());
+  template = replace(template, "id", manager.getId());
+  template = replace(template, "officeNumber", manager.getOfficeNumber());
+  return template;
+};
+
+const displayEngineer = (engineer) => {
+  let template = fs.readFileSync(path.resolve(templatesDir, "engineer.html"), "utf8");
+  template = replace(template, "name", engineer.getName());
+  template = replace(template, "role", engineer.getRole());
+  template = replace(template, "email", engineer.getEmail());
+  template = replace(template, "id", engineer.getId());
+  template = replace(template, "github", engineer.getGithub());
+  return template;
+};
+
+const displayIntern = (intern) => {
+  let template = fs.readFileSync(path.resolve(templatesDir, "intern.html"), "utf8");
+  template = replace(template, "name", intern.getName());
+  template = replace(template, "role", intern.getRole());
+  template = replace(template, "email", intern.getEmail());
+  template = replace(template, "id", intern.getId());
+  template = replace(template, "school", intern.getSchool());
+  return template;
+};
+
+const displayMain = (html) => {
+  const template = fs.readFileSync(path.resolve(templatesDir, "team.html"), "utf8");
+  return replace(template, "team", html);
+};
+
+const replace = (template, placeholder, value) => {
+  const pattern = new RegExp("{{ " + placeholder + " }}", "gm");
+  return template.replace(pattern, value);
+};
+
+module.exports = display;
---
+++ index.js
@@ -0,0 +1,95 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+
+// files in directories file paths
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const display = require("./lib/htmlGenerator");
+
+
+const PATH_DIR = path.resolve(__dirname, "dist");
+const outputPath = path.join(PATH_DIR, "index.html");
+
+function validate(input) {
+  if (!input == "") {
+    return true;
+  } else {
+    return "Field cannot be blank";
+  }
+}
+
+// propmting user with questions
+const questions = {
+  type: function () {
+    return {
+      message: "Please confirm team memeber you would like to add?",
+      type: "list",
+      name: "member",
+      choices: ["Engineer", "Intern", "No more to add"],
+    };
+  },
+  item: function (member, variable, item = variable, validate) {
+    return {
+      message: `What is your ${member.toLowerCase()}'s ${item}?`,
+      type: "input",
+      name: variable,
+      validate: validate,
+    };
+  },
+};
+async function generateHTML() {
+  const html = display(employees);
+
+  if (!fs.existsSync(PATH_DIR)) {
+    fs.mkdirSync(PATH_DIR);
+  }
+  fs.writeFileSync(outputPath, html, "utf-8");
+  console.log("Successfully created an index.html page!");
+}
+
+let employees = [];
+
+async function addEmployee(member) {
+  
+  let { name } = await inquirer.prompt(questions.item(member, "name", "name", validate));
+  let { id } = await inquirer.prompt(questions.item(member, "id", "ID number", validate));
+  let { email } = await inquirer.prompt(questions.item(member, "email", "email address", validate));
+  
+  switch (member) {
+    case "Manager":
+      let { officeNumber } = await inquirer.prompt(
+        questions.item(member, "officeNumber", "office phone number", validate)
+      );
+      employees.push(new Manager(name, id, email, officeNumber));
+      break;
+    case "Engineer":
+      let { github } = await inquirer.prompt(questions.item(member, "github", "GitHub username", validate));
+      employees.push(new Engineer(name, id, email, github));
+      break;
+    case "Intern":
+      let { school } = await inquirer.prompt(questions.item(member, "school", "school", validate));
+      employees.push(new Intern(name, id, email, school));
+      break;
+
+      default:
+        addManager();
+  }
+}
+
+async function addManager() {
+  console.log("Lets start building your team");
+  await addEmployee("Manager");
+  let member = "";
+  let exit = "No more to add";
+  while (member != exit) {
+    let { member } = await inquirer.prompt(questions.type());
+    if (member === exit) {
+      return generateHTML();
+    }
+    await addEmployee(member);
+  }
+}
+
+addManager();
